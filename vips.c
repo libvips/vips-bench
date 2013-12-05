fa2ab4194f65a2b498c1457402b1ee50b5b452f2
John Cupitt <jcupitt@gmail.com>
2013-12-05 11:00:09 +0000

    vips8 C version

--- vips.c
+++ vips.c
@@ -1,44 +1,47 @@
 // compile with
-// gcc vips.c `pkg-config vips --cflags --libs`
+// gcc -Wall vips.c `pkg-config vips --cflags --libs` -o vips-c
 
 #include <vips/vips.h>
 
 int 
-main (int argc, char **argv)
+main( int argc, char **argv )
 {
+	GOptionContext *context;
 	VipsImage *in;
 	VipsImage **t;
-	INTMASK *m;
+
+	GError *error = NULL;
+
+	if( vips_init( argv[0] ) )
+		return( -1 );
+
+	context = g_option_context_new( " - VIPS benchmark program" );
+	g_option_context_add_group( context, vips_get_option_group() );
+	if( !g_option_context_parse( context, &argc, &argv, &error ) ) {
+		if( error ) {
+			fprintf( stderr, "%s\n", error->message );
+			g_error_free( error );
+		}
+
+		vips_error_exit( NULL );
+	}
 
 	if( !(in = vips_image_new_mode( argv[1], "rs" )) )
 		vips_error_exit( "unable to read %s", argv[1] );
-	t = (VipsImage **) vips_object_local_array( in, 4 );
+	t = (VipsImage **) vips_object_local_array( VIPS_OBJECT( in ), 4 );
 
-	m = im_create_imaskv( "conv.mat", 3, 3, 
-                -1, -1, -1, -1, 16,-1, -1, -1, -1 );
-	m->scale = 8;
+	t[0] = vips_image_new_matrixv( 3, 3, 
+		-1, -1, -1, -1, 16,-1, -1, -1, -1 );
+	vips_image_set_double( t[0], "scale", 8 ); 
 
-	if( vips_extract_area( in, &t[0], 
-                100, 100, in.Xsize () - 200, in.Ysize () - 200, NULL ) ||
-		vips_affine( t[0], &t[1], 0.9, 0, 0, 0.9, NULL ) ||
-		im_conv( 
+	if( vips_extract_area( in, &t[1], 
+                100, 100, in->Xsize - 200, in->Ysize - 200, NULL ) ||
+		vips_affine( t[1], &t[2], 0.9, 0, 0, 0.9, NULL ) ||
+		vips_conv( t[2], &t[3], t[0], NULL ) ||
+		vips_image_write_to_file( t[3], argv[2] ) )
+		return( -1 );
 
+	g_object_unref( in );
 
-        vips::VImage in (argv[1]);
-	m = im_create_imaskv( "conv.mat", 3, 3, 
-                -1, -1, -1, -1, 16,-1, -1, -1, -1 );
-	m->scale = 8;
-
-        vips::VIMask mask (3, 3, 8, 0,
-                -1, -1, -1, -1, 16,-1, -1, -1, -1);
-
-        in.
-                extract_area (100, 100, in.Xsize () - 200, in.Ysize () - 200).
-                affine (0.9, 0, 0, 0.9, 0, 0,
-                        0, 0, in.Xsize () * 0.9, in.Ysize () * 0.9).
-                conv (mask).
-                write (argv[2]);
-
-        return 0;
+	return( 0 );
 }
-
