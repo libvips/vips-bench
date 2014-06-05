5dddcd63f24b9932c4070d90b0168f519358a5d6
John Cupitt <jcupitt@gmail.com>
2014-06-05 14:50:15 +0100

    oops comma

--- ruby-vips.rb
+++ ruby-vips.rb
@@ -10,7 +10,7 @@ im = im.extract_area(100, 100, im.x_size - 200, im.y_size - 200)
 im = im.affinei(:bilinear, 0.9, 0, 0, 0.9, 0, 0)
 mask = [
 	[-1, -1,  -1], 
-	[-1,  16, -1,],
+	[-1,  16, -1],
 	[-1, -1,  -1]
 ]
 m = VIPS::Mask.new mask, 8, 0
