c40411bad02a2825761797a32f17bb264524d7bf
John Cupitt <jcupitt@gmail.com>
2014-11-06 13:33:52 +0000

    tiny improvement for pyvips8

--- vips8.py
+++ vips8.py
@@ -6,8 +6,8 @@ from gi.repository import Vips
 
 im = Vips.Image.new_from_file(sys.argv[1])
 
-im = im.crop(100, 100, im.width - 200, im.height - 200)
-im = im.affine([0.9, 0, 0, 0.9])
+im = im.extract_area(100, 100, im.width - 200, im.height - 200)
+im = im.similarity(scale = 0.9)
 mask = Vips.Image.new_from_array([[-1, -1,  -1], 
                                   [-1,  16, -1], 
                                   [-1, -1,  -1]], scale = 8)
