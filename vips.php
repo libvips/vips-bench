cc9438bd6ab65fdb3cff3eab5748f045039cdaff
John Cupitt <jcupitt@gmail.com>
2016-09-21 11:37:51 +0100

    update for latest vips-php

--- vips.php
+++ vips.php
@@ -1,20 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-include '../vips.php';
+include 'Image.php';
 
-$im = VImage::new_from_file($argv[1], ["access" => "sequential"]);
+use JCupitt\Vips;
+
+$im = Vips\Image::newFromFile($argv[1], ["access" => "sequential"]);
 
 $im = $im->crop(100, 100, $im->width - 200, $im->height - 200);
 
-$im = $im->similarity(["scale" => 0.9]);  
+$im = $im->reduce(1.0 / 0.9, 1.0 / 0.9, ["kernel" => "linear"]);
 
-$mask = VImage::new_from_array(
+$mask = Vips\Image::newFromArray(
 		  [[-1,  -1, -1], 
 		   [-1,  16, -1], 
 		   [-1,  -1, -1]], 8);
 $im = $im->conv($mask);
 
-$im->write_to_file($argv[2]);
+$im->writeToFile($argv[2]);
 
 ?>
