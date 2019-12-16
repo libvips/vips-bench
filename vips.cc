512b95865f14ab31f1843de911740e55c1b0b064
John Cupitt <jcupitt@gmail.com>
2019-12-16 14:28:19 +0000

    fix varargs problem in vips.cc

--- vips.cc
+++ vips.cc
@@ -15,21 +15,21 @@ main( int argc, char **argv )
 	if( VIPS_INIT( argv[0] ) )
 		return( -1 );
 
-    VImage in = VImage::new_from_file( argv[1], 
-		VImage::option()-> set( "access", VIPS_ACCESS_SEQUENTIAL ));
+	VImage in = VImage::new_from_file( argv[1], 
+		VImage::option()-> 
+			set( "access", VIPS_ACCESS_SEQUENTIAL ));
 
-    VImage mask = VImage::new_matrixv( 3, 3, 
-        -1, -1, -1, -1, 16, -1, -1, -1, -1 );
+	VImage mask = VImage::new_matrixv( 3, 3, 
+		-1.0, -1.0, -1.0, -1.0, 16.0, -1.0, -1.0, -1.0, -1.0 );
 	mask.set( "scale", 8 ); 
 
-    in.
-        extract_area( 100, 100, in.width() - 200, in.height() - 200 ).
-		reduce( 1.0 / 0.9, 1.0 / 0.9, VImage::option()->
-			set( "kernel", VIPS_KERNEL_LINEAR ) ).
-        conv( mask, VImage::option()->
-			set( "precision", VIPS_PRECISION_INTEGER )). 
+	in
+		.extract_area( 100, 100, in.width() - 200, in.height() - 200 )
+		.reduce( 1.0 / 0.9, 1.0 / 0.9, VImage::option()->
+			set( "kernel", VIPS_KERNEL_LINEAR ) )
+		.conv( mask, VImage::option()->
+			set( "precision", VIPS_PRECISION_INTEGER ))
+		.write_to_file( argv[2] );
 
-    write_to_file( argv[2] );
-
-    return( 0 );
+	return( 0 );
 }
